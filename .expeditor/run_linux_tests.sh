4f8c22e10551a20f9241c6ed979a09ddddd871fd
nikhil2611 <nikhilgupta2102@gmail.com>
2025-07-25 19:13:57 +0530

    reverting back the image to ruby:3.1
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- .expeditor/run_linux_tests.sh
+++ .expeditor/run_linux_tests.sh
@@ -8,13 +8,5 @@ echo "--- bundle install"
 bundle config --local path vendor/bundle
 bundle install --jobs=7 --retry=3
 
-# echo "--- rebuild ffi native extension if present"
-# if bundle list | grep -q 'ffi'; then
-#   echo "Rebuilding ffi from source..."
-#   bundle pristine ffi
-# else
-#   echo "ffi gem not found, skipping rebuild"
-# fi
-
 echo "+++ bundle exec task"
 bundle exec "$@"
